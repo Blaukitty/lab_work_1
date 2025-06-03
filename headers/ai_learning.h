a9c4a6d253fa787249699107d44220f0f4d94e82
Rodionova Ksenia <st132496@student.spbu.ru>
2025-06-03 12:59:58 +0300

    Update ai_learning.h

--- headers/ai_learning.h
+++ headers/ai_learning.h
@@ -7,15 +7,13 @@
 #include <algorithm>
 #include <iostream>
 
-using namespace std;
-
 struct aiCardData {
-    string name2;
+    std::string name2;
     int data2;
 };
 
 class ai_learning {
-    vector<aiCardData> sel_ai_cards;
+    std::vector<aiCardData> sel_ai_cards;
     int counter = 0;
 
 public:
